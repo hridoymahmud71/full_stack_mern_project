8ed6387e91d363e6e478482f92cd210014407dd6
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-01-18 16:04:15 +0600

    before relationship

---
+++ graphql_queries.md
@@ -0,0 +1,46 @@
+#### Get single client
+``` 
+{
+  client(id:"1"){
+    id
+    name
+    email
+    phone
+  }
+}
+```
+
+#### Get All clients
+``` 
+{
+  clients{
+    id
+    name
+    email
+    phone
+  }
+}
+```
+
+#### Get single project
+``` 
+{
+  client(id:"1"){
+    id
+    name
+    email
+    phone
+  }
+}
+```
+
+#### Get All projects
+``` 
+{
+  projects {
+    name
+    status
+    description
+  }
+}
+```
--- server/index.js
+++ server/index.js
@@ -1,6 +1,23 @@
 // external imports
-const express  = require("express");
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+
+// internal exports
+const schema = require("./schema/schema");
+
+// configuarations
+const port = process.env.PORT || 5000;
 
 
+const app = express();
 
-const app  = express;
+app.use("/graphql", graphqlHTTP(
+    {
+        schema, 
+        graphiql: process.env.NODE_ENV === "development"
+    }
+))
+
+// listen to port
+app.listen(port, console.log(`Server running on port ${port}`))
