beafede752d120fc53f5f75efac84708de271b14
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-01-19 17:05:02 +0600

    client deleted

--- client/src/components/ClientRow.js
+++ client/src/components/ClientRow.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from './../mutations/clientMutations';
 
-function ClientRow({client}) {
+
+function ClientRow({ client }) {
+
+    const [deleteClient] = useMutation(DELETE_CLIENT, { variables: { id: client.id } })
+
     return (
         <tr key={client.id}>
             <td>{client.name}</td>
@@ -9,7 +15,7 @@ function ClientRow({client}) {
             <td>{client.phone}</td>
             <td>
                 <button className='btn btn-danger btn-sm' >
-                    <FaTrash />
+                    <FaTrash onClick={deleteClient} />
                 </button>
             </td>
         </tr>
